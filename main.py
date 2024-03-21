a0788e661673864c70ea41b039148ebae87a119a
Jericho R Orienza <93217801+Jericho-R-Orienza@users.noreply.github.com>
2024-03-21 18:29:21 -0400

    added button class to create intro screen and play option

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class Game:
         self.character_spritesheet = Spritesheet('img/character.png')
         self.terrain_spritesheet = Spritesheet('img/terrain.png')
         self.enemy_spritesheet = Spritesheet('img/enemy.png')
+        self.intro_background = pygame.image.load('./img/introbackground.png')
 
     def createTileMap(self):
         for i, row in enumerate(tilemap):
@@ -68,7 +69,30 @@ class Game:
         pass
 
     def intro_screen(self):
-        pass
+        intro = True
+
+        title = self.font.render('DEMO GAME', True, BLACK)
+        title_rect = title.get_rect(x=10, y=10)
+
+        play_button = Button(10, 50, 100, 50, WHITE, BLACK, 'PLAY', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed() #0 left click, 1 right click, 2 middle click
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+
+            pygame.display.update()
 
 g = Game()
 g.intro_screen() 
