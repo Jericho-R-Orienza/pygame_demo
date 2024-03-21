d2adcaaecc58e2d7a13af7db90ec75ec9dc74647
Jericho R Orienza <93217801+Jericho-R-Orienza@users.noreply.github.com>
2024-03-21 02:33:21 -0400

    added PLayer movement animation

--- sprites.py
+++ sprites.py
@@ -35,7 +35,8 @@ class Player(pygame.sprite.Sprite):
         self.x_change = 0
         self.y_change = 0
 
-        self.facing = 'down'
+        self.facing = 'down' #default facing
+        self.animation_loop = 1
 
         self.image = self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height)
 
@@ -45,6 +46,8 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.movement()
+        self.animate()
+
         self.rect.x += self.x_change
         self.collide_blocks('x')
         self.rect.y += self.y_change
@@ -88,6 +91,59 @@ class Player(pygame.sprite.Sprite):
                 if self.y_change < 0: #collide above
                     self.rect.y = hits[0].rect.bottom
 
+    def animate(self):
+        down_animations = [self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(35, 2, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(68, 2, self.width, self.height)]
+
+        up_animations = [self.game.character_spritesheet.get_sprite(3, 34, self.width, self.height),
+                         self.game.character_spritesheet.get_sprite(35, 34, self.width, self.height),
+                         self.game.character_spritesheet.get_sprite(68, 34, self.width, self.height)]
+
+        left_animations = [self.game.character_spritesheet.get_sprite(3, 98, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(35, 98, self.width, self.height),
+                           self.game.character_spritesheet.get_sprite(68, 98, self.width, self.height)]
+
+        right_animations = [self.game.character_spritesheet.get_sprite(3, 66, self.width, self.height),
+                            self.game.character_spritesheet.get_sprite(35, 66, self.width, self.height),
+                            self.game.character_spritesheet.get_sprite(68, 66, self.width, self.height)]
+
+        if self.facing == "down":
+            if self.y_change == 0: #standing still
+                self.image = self.game.character_spritesheet.get_sprite(3, 2, self.width, self.height)
+            else:
+                self.image = down_animations[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1 #we are reaching 1, 2, 3 in our array every 0.1->10 frames
+                if self.animation_loop >= 3: #reset back to 1 -> looping the animation
+                    self.animation_loop = 1
+
+        if self.facing == "up":
+            if self.y_change == 0: #standing still
+                self.image = self.game.character_spritesheet.get_sprite(3, 34, self.width, self.height)
+            else:
+                self.image = up_animations[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1 #we are reaching 1, 2, 3 in our array every 0.1->10 frames
+                if self.animation_loop >= 3: #reset back to 1 -> looping the animation
+                    self.animation_loop = 1
+
+        if self.facing == "left":
+            if self.x_change == 0: #standing still
+                self.image = self.game.character_spritesheet.get_sprite(3, 98, self.width, self.height)
+            else:
+                self.image = left_animations[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1 #we are reaching 1, 2, 3 in our array every 0.1->10 frames
+                if self.animation_loop >= 3: #reset back to 1 -> looping the animation
+                    self.animation_loop = 1
+
+        if self.facing == "right":
+            if self.x_change == 0: #standing still
+                self.image = self.game.character_spritesheet.get_sprite(3, 66, self.width, self.height)
+            else:
+                self.image = right_animations[math.floor(self.animation_loop)]
+                self.animation_loop += 0.1 #we are reaching 1, 2, 3 in our array every 0.1->10 frames
+                if self.animation_loop >= 3: #reset back to 1 -> looping the animation
+                    self.animation_loop = 1
+        
 class Block(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
         self.game = game
